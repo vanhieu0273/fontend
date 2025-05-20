506e8a581135162f49f22c510efdec237f39138d
hieu <2151060261@e.tlu.edu.vn>
2025-05-20 19:38:56 +0700

    update

---
+++ src/components/CartTotal.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContextReal'
+import Title from './Title'
+
+const CartTotal = () => {
+
+    const {currency, delivery_free, getCartAmount} = useContext(ShopContext)
+
+  return (
+    <div className='w-full '>
+        <div className='text-2xl'>
+            <Title text1={'CART'} text2={'TOTALS'}/>
+        </div>
+        <div className='flex flex-col gap-2 mt-2 text-sm'>
+            <div className='flex justify-between'>
+                <p>SubTotal</p>
+                <p>{currency} {getCartAmount()}.00</p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p>Shipping fee </p>
+                <p>{currency} {delivery_free}.00</p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <b>Total</b>
+                <b>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_free}.00</b>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CartTotal
--- src/context/ShopContextReal.jsx
+++ src/context/ShopContextReal.jsx
@@ -20,9 +20,11 @@
 
 // export default ShopContextProvider;
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import React from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -32,17 +34,82 @@ export const ShopContextProvider = (props) => {
   
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(true);
+  const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
+
+  const addToCart = async (itemId, size) => {
+    if(!size){
+      toast.error('Please select a size');
+      return;
+    }
+
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId]) {
+      if(cartData[itemId][size]){
+        cartData[itemId][size] += 1;
+      }else{
+        cartData[itemId][size] = 1;
+      }
+    }else{
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  }
+
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems){
+      for(const item in cartItems[items]){
+        try {
+          if(cartItems[items][item] > 0){
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalCount;
+  }
+
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  }
+
+  const getCartAmount =  () => {
+    let totalAmount = 0;
+
+    for(const items in cartItems){
+      let itemInfo = products.find((product) => product._id === items);
+      for(const item in cartItems[items]){
+        try {
+          if(cartItems[items][item] > 0){
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalAmount;
+  }
+
+  // useEffect(() => {
+  //   console.log('cartItems', cartItems);
+    
+  // }, [cartItems])
 
   
 
   const value = {
-    products,
-    currency,
-    delivery_free,
-    search, 
-    setSearch,
-    showSearch,
-    setShowSearch
+    products,currency,delivery_free,
+    search, setSearch,showSearch,setShowSearch,
+    cartItems, addToCart,
+    getCartCount,updateQuantity,
+    getCartAmount, navigate
   };
 
   return (
